690cdf32b74f32be828b544a7558dd96b76d3d63
Sam Rose <sam@floxdev.com>
2022-11-02 16:37:17 -0400

    chore: cleanup pkg

--- pkgs/django-poetry-example/default.nix
+++ pkgs/django-poetry-example/default.nix
@@ -1,6 +1,4 @@
-# Replace "stdenv" with the namespace or name of your language's builder
 { self, poetry2nix }:
-# Replace "stdenv.mkDerivation" with your language's builder
 poetry2nix.mkPoetryApplication {
   projectDir = self;
 }
